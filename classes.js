b1b10fde9e6d3f93f9fcb1268350304065b92ce0
cj2406 <joeltimicampbell@gmail.com>
2025-09-26 16:13:37 +0100

    Add files via upload

---
+++ classes.js
@@ -0,0 +1,46 @@
+/*
+  so in JavaScript, a class is a blueprint for creating objects with
+  shared properties and methods.
+ */
+
+class Person {
+  // this is a constructor(runs setup code for a class).
+  // it is Special method called when creating an object with new
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  // Method basically this is a function inside an object/class
+  greet() {
+    console.log(`Hello, my name is ${this.name} and I’m ${this.age} years old.`);
+  }
+
+}
+
+// Creating objects (instances)
+const person1 = new Person("mirabel", 25);
+person1.greet();  // Hello, my name is mirabel and I’m 25 years old.
+
+
+
+//In classes we have what we call Inheritance where a class gets a property or
+//  method from its parent class
+
+class People extends Person {
+  constructor(name, age) {
+    // the super keyword Call the parent (Person) constructor with name and age
+    super(name, age);
+  }
+
+  //method
+  shout() {
+    console.log("Hey!!!");
+  }
+}
+
+const person2 = new People("mary", 19);
+person2.greet(); // Hello, my name is mary and I’m 19 years old.
+person2.shout(); // Hey!!!
+
+
