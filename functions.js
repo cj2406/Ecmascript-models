6dd9ad5383a82fb0889b8dc61fccee49632672dd
cj2406 <joeltimicampbell@gmail.com>
2025-09-26 14:14:29 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,11 @@
+//THIS IS A FILE containing basic functions which i will use to demonstrate modules
+
+export function displayNumbers(){
+    let numbers=[1,5,7,3,9,0,7];
+    console.log(numbers);
+}
+
+export const displayGrades= ()=>{
+    let grades=['A','B','C','E','F'];
+    console.log(`the grade is ${grades}`)
+}
